6e7a2dc691630e46df848ffb64fbe4fbf38e3c52
PatAm16 <patricia.amaral16@gmail.com>
2021-03-15 23:54:14 +0000

    Update EscolheACarta.cs

--- Assets/EscolheACarta.cs
+++ Assets/EscolheACarta.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class EscolheACarta : MonoBehaviour
 {
     int NumerosDasCartas;
-
+    int Naipes;
+    string NomeDosNaipes;
+    string Figuras;
+  
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +23,63 @@ public class EscolheACarta : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-            NumerosDasCartas = Random.Range(1, 10);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NumerosDasCartas = Random.Range(1, 15);
+            Naipes = Random.Range(1, 5);
+
+            if (Naipes == 1)
+            {
+                NomeDosNaipes = "Copas";
+            }
+
+            if (Naipes == 2)
+            {
+                NomeDosNaipes = "Ouros";
+            }
+
+            if (Naipes == 3)
+            {
+                NomeDosNaipes = "Espadas";
+            }
+
+            if (Naipes == 4)
+            {
+                NomeDosNaipes = "Paus";
+            }
+
+            if (NumerosDasCartas == 11)
+            {
+                Figuras = "Rei";
+            }
+
+            if (NumerosDasCartas == 12)
+            {
+                Figuras = "Valete";
+            }
+
+            if (NumerosDasCartas == 13)
+            {
+                Figuras = "Dama";
+            }
+
+            if (NumerosDasCartas == 14)
+            {
+                Figuras = "Ás";
+            }
+
+            if (NumerosDasCartas > 10)
+            {
+                Debug.Log("A carta que te saiu foi " + Figuras + " de " + NomeDosNaipes);
+            }
+            else
+            {
+                Debug.Log("A carta que te saiu foi " + NumerosDasCartas + " de " + NomeDosNaipes);
+            }
+            
         }
 
-        Debug.Log("A carta que te saiu foi" + NumerosDasCartas);
+
+        
     }
 }
