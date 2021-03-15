14782ee09b40587db288d4d0c75c2e5e279d549c
PatAm16 <patricia.amaral16@gmail.com>
2021-03-15 21:43:48 +0000

    Uptade EscolheACarta.cs
    Introdução ao jogo

--- Assets/EscolheACarta.cs
+++ Assets/EscolheACarta.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class EscolheACarta : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Olá, vamos divertir-nos!!");
+        Debug.Log("Carrega na tecla espaço para te sair uma carta");
+
     }
 
     // Update is called once per frame
